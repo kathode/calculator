991388710c754f6f65b08030fb50b8e0b59fe74c
Kathode <dtu@deakin.edu.au>
2024-08-11 00:09:16 +1000

    Add calculator UI

--- src/index.js
+++ src/index.js
@@ -36,6 +36,80 @@ class Calculator {
         throw new Error("Invalid operation.");
     }
   }
+
+  static generateUI() {
+    const app = document.querySelector(".app");
+
+    const container = document.createElement("div");
+    container.className = "calculator";
+    app.appendChild(container);
+
+    const displayContainer = document.createElement("div");
+    displayContainer.className = "display-container";
+    container.appendChild(displayContainer);
+
+    const displayValue = document.createElement("div");
+    displayValue.className = "display-value";
+    displayValue.textContent = "0";
+    displayContainer.appendChild(displayValue);
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.className = "button-container";
+    container.appendChild(buttonContainer);
+
+    const buttons = [
+      [
+        { name: "AC", fill: 1, color: "#626261" },
+        { name: "+/-", fill: 1, color: "#626261" },
+        { name: "%", fill: 1, color: "#626261" },
+        { name: "÷", fill: 1, color: "#FF9F0A" },
+      ],
+
+      [
+        { name: "7", fill: 1, color: "#7C7C7B" },
+        { name: "8", fill: 1, color: "#7C7C7B" },
+        { name: "9", fill: 1, color: "#7C7C7B" },
+        { name: "×", fill: 1, color: "#FF9F0A" },
+      ],
+
+      [
+        { name: "4", fill: 1, color: "#7C7C7B" },
+        { name: "5", fill: 1, color: "#7C7C7B" },
+        { name: "6", fill: 1, color: "#7C7C7B" },
+        { name: "-", fill: 1, color: "#FF9F0A" },
+      ],
+
+      [
+        { name: "1", fill: 1, color: "#7C7C7B" },
+        { name: "2", fill: 1, color: "#7C7C7B" },
+        { name: "3", fill: 1, color: "#7C7C7B" },
+        { name: "+", fill: 1, color: "#FF9F0A" },
+      ],
+
+      [
+        { name: "0", fill: 2, color: "#7C7C7B" },
+        { name: ".", fill: 1, color: "#7C7C7B" },
+        { name: "=", fill: 1, color: "#FF9F0A" },
+      ],
+    ];
+
+    for (const rows of buttons) {
+      const row = document.createElement("div");
+      row.className = "calculator-button-row";
+
+      for (const config of rows) {
+        const button = document.createElement("div");
+        button.textContent = config.name;
+        button.className = "calculator-button";
+        button.style.background = config.color;
+        button.style.width = `${25 * config.fill}%`;
+        row.appendChild(button);
+      }
+      buttonContainer.appendChild(row);
+    }
+  }
 }
+
+Calculator.generateUI();
 
 module.exports = Calculator;
